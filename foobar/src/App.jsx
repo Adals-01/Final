c4cf663aaf3dbe155e557dee273c8877d1c246b5
Elise <aemccreary5@gmail.com>
2021-12-01 19:51:44 +0100

    data working

--- foobar/src/App.jsx
+++ foobar/src/App.jsx
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
 
-   
-
-
 function App() {
-
   const [data, setItems] = useState([]);
 
   useEffect(() => {
-
     const fetchItems = async () => {
       try {
         const response = await fetch(`https://foobar-team10.herokuapp.com/`);
@@ -18,58 +13,53 @@ function App() {
       } catch (err) {
         console.log(err);
       }
-    }
+    };
     const id = setInterval(() => {
       fetchItems();
     }, 25000);
     fetchItems();
     return () => clearInterval(id);
-
   }, []);
-  
+
   function Overview(props) {
     return (
-   <section> 
-     <div>Hello</div>
-   <p> time: {props.timestamp}</p>
-   
- {/*  <h1>Hello {props.bar.name} </h1>  
-<p> Tap: {props.storage[0].name}</p>  */}
-    </section> 
-    );
-  }   
-
-
-
-/*   function StorageList(props) {
-    const StorageList = [props.storage]
-    const newArray = storage.map((storage) => <StorageItem  key={storage.id} {...storage} />);
-    return (
       <section>
-        <h1> Productlist </h1>
-        <section className="Productlist">{newArray}</section>
+        <div>Hello</div>
+        <p> time: {props.data && props.data.timestamp}</p>
+
+        <h1>Hello {props.data.bar && props.data.bar.name} </h1>
+        <p> Tap: {props.data.storage && props.data.storage[0].name}</p>
       </section>
     );
   }
-   */
-  function StorageItem() {
-     return (
-      <section>
-        <h1> Productlist </h1>
-      </section>)
-  }
 
-  
+  // function StorageList(props) {
+  //   const StorageList = [props.storage];
+  //   const newArray = storage.map((storage) => <StorageItem key={storage.id} {...storage} />);
+  //   return (
+  //     <section>
+  //       <h1> Productlist </h1>
+  //       <section className="Productlist">{newArray}</section>
+  //     </section>
+  //   );
+  // }
+
+  // function StorageItem() {
+  //   return (
+  //     <section>
+  //       <h1> Productlist </h1>
+  //     </section>
+  //   );
+  // }
+
   return (
     <div className="App">
       <div>
-      {data && <Overview  data={data}/> }
-{/*   {data && <StorageList storage ={data.storage}/> }    */}
+        <Overview data={data} />
+        {/* <StorageList storage={data.storage} /> */}
       </div>
     </div>
   );
-  }
-
-
+}
 
 export default App;
