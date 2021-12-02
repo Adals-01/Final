71fb4729228d47d41c53d334f4ac47752215aacf
Elise <aemccreary5@gmail.com>
2021-12-02 14:55:47 +0100

    order tickets and overview

--- foobar/src/App.jsx
+++ foobar/src/App.jsx
@@ -22,111 +22,116 @@ function App() {
   }, []);
 
   function Overview(props) {
-    
     return (
       <section className="overview">
-        <h1>Hello</h1>
-      <p> time: {props.data.timestamp}</p> 
+        {/* <p> time: {props.data.timestamp}</p> */}
 
-     <h1>Hello {props.data.bar && props.data.bar.name} </h1>
-        <p> Tap: {props.data.storage && props.data.storage[0].name}</p>
+        <h1>Good Evening, {props.data.bar && props.data.bar.name} Team!</h1>
+        <div className="stats">
+          <div className="statsItem">
+            <div className="statNumber">?</div>
+            <p> Orders Completed </p>
+          </div>
+          <div className="statsItem">
+            <div className="statNumber">{props.data.queue && props.data.queue.length}</div>
+            <p> Orders in Line</p>
+          </div>
+          <div className="statsItem">
+            <div className="statNumber">{props.data.taps && props.data.taps.length}</div>
+            <p> Beers on Tap </p>
+          </div>
+        </div>
       </section>
     );
   }
 
   function StorageList(props) {
+    console.log(props.storage);
+    const storageArr = props.storages.map((storage, index) => <StorageItem key={index} {...storage} />);
+    return <section className="storage-list">{storageArr}</section>;
+  }
 
-  console.log(props.storage);
-  const storageArr = props.storages.map((storage, index) => <StorageItem key={index} {...storage} />); 
-  return  (
-    <section className="storage-list">
-     {storageArr}
-      </section>
-  )}
-
-function StorageItem(props) {
-  return (
-    <div className="storage-item">
-    <h2>{props.name}</h2>
-    <p>{props.amount}</p>
-    </div>
-  )
-}
+  function StorageItem(props) {
+    return (
+      <div className="storage-item">
+        <h2>{props.name}</h2>
+        <p>{props.amount}</p>
+      </div>
+    );
+  }
   function TapList(props) {
-  console.log(props.taps);
-  const tapArr = props.taps.map((tap) => <TapItem key={tap.id} {...tap} />); 
-  return  (
-    <section className="tap-list">
-     {tapArr}
-      </section>
-  )}
+    console.log(props.taps);
+    const tapArr = props.taps.map((tap) => <TapItem key={tap.id} {...tap} />);
+    return <section className="tap-list">{tapArr}</section>;
+  }
 
-function TapItem(props) {
-  return (
-    <div className="tap-item">
-    <h2>{props.beer}</h2>
-    <p>{props.level}</p>
-    <p>{props.capacity}</p>
-    </div>
-  )
-}
+  function TapItem(props) {
+    return (
+      <div className="tap-item">
+        <h2>{props.beer}</h2>
+        <p>{props.level}</p>
+        <p>{props.capacity}</p>
+      </div>
+    );
+  }
 
   function QueueList(props) {
-  const queueArr = props.queue.map((queue, index) => <QueueItem key={index} {...queue} />); 
-  return  (
-    <section className="queue-list">
-     {queueArr}
-      </section>
-  )}
+    const queueArr = props.queue.map((queue, index) => <QueueItem key={index} {...queue} />);
+    return <section className="queue-list">{queueArr}</section>;
+  }
 
-function QueueItem(props) {
-  /*  const queueItemArr = props.order.map((order, index) => <QueueItemOrder key={index} order={order} />); */
-  return (
-    <div className="queue-item">
-    <h2>#{props.id}</h2>
-{/*    <div>{queueItemArr}</div> */}
-  {props.order.map((order, index) => <div key={index}>{order}</div>)} 
-    </div>
-  )
-}
+  function QueueItem(props) {
+    /*  const queueItemArr = props.order.map((order, index) => <QueueItemOrder key={index} order={order} />); */
+    return (
+      <div className="queue-item">
+        <h3>#{props.id}</h3>
+        {/*    <div>{queueItemArr}</div> */}
+        <div className="dateTime">
+          <p>{props.startTime}</p>
+        </div>
+        {props.order.map((order, index) => (
+          <li key={index}>{order}</li>
+        ))}
+      </div>
+    );
+  }
   function ServingList(props) {
-  const servingArr = props.serving.map((serving, index) => <ServingItem key={index} {...serving} />); 
-  return  (
-    <section className="queue-list">
-     {servingArr}
-      </section>
-  )}
+    const servingArr = props.serving.map((serving, index) => <ServingItem key={index} {...serving} />);
+    return <section className="queue-list">{servingArr}</section>;
+  }
 
-function ServingItem(props) {
-  /*  const queueItemArr = props.order.map((order, index) => <QueueItemOrder key={index} order={order} />); */
-  return (
-    <div className="queue-item">
-    <h2>SERVING #{props.id}</h2>
-{/*    <div>{queueItemArr}</div> */}
-  {props.order.map((order, index) => <div key={index}>{order}</div>)} 
-    </div>
-  )
-}
+  function ServingItem(props) {
+    /*  const queueItemArr = props.order.map((order, index) => <QueueItemOrder key={index} order={order} />); */
+    return (
+      <div className="serving-item">
+        <h3>#{props.id}</h3>
+        {/*    <div>{queueItemArr}</div> */}
+        <div className="dateTime">
+          <p>{props.startTime}</p>
+        </div>
 
+        {props.order.map((order, index) => (
+          <li key={index}>{order}</li>
+        ))}
+      </div>
+    );
+  }
 
-
-
- 
   return (
     <div className="App">
       <main>
-      <aside>{data.taps && <TapList taps={data.taps} /> }</aside>
+        <aside>{data.taps && <TapList taps={data.taps} />}</aside>
         <div className="right-side">
           <div className="overview-logo">
             <Overview data={data} />
             <div>Logo</div>
           </div>
           <div className="serving-and-queue">
-            {data.serving && <ServingList serving={data.serving} /> }
-            {data.queue && <QueueList queue={data.queue} /> }
+            {data.serving && <ServingList serving={data.serving} />}
+            {data.queue && <QueueList queue={data.queue} />}
           </div>
-        {data.storage && <StorageList storages={data.storage} /> }
-      </div>
+          {data.storage && <StorageList storages={data.storage} />}
+        </div>
       </main>
     </div>
   );
