c1156f5a90aa5c8de63bc25420604430dbfe58b7
Anna <anna355y@stud.kea.dk>
2021-12-15 15:28:05 +0100

    stet 3 checkout changes

--- foobar/src/components/Order/stepthree.jsx
+++ foobar/src/components/Order/stepthree.jsx
@@ -1,11 +1,14 @@
 
-export default function StepThree(props) {
+import leaf1 from "../../assets/illustrations/leaf1.svg";
 
+export default function StepThree(props)
+ {
     return (
         <div className="form-container step-three">
         <h2>Thank you for ordering!</h2>
         <p>The bartenders have been notified, and your drinks will be ready soon.</p>
         <button>WANT MORE SIPS?</button>
+        <img className="left" src={leaf1} alt={"hop leaf"} />
         </div>
      )
  }
