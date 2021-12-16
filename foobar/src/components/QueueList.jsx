9cfa21eecefcf38554818117e9477fb31407a92d
Anna <anna355y@stud.kea.dk>
2021-12-16 12:31:11 +0100

    navbar styling

--- foobar/src/components/QueueList.jsx
+++ foobar/src/components/QueueList.jsx
@@ -1,16 +1,11 @@
 import QueueItem from "./QueueItem";
-import FlipMove from 'react-flip-move';
+import FlipMove from "react-flip-move";
 
 export default function QueueList(props) {
   const queueArr = props.queue.map((queue, index) => <QueueItem key={index} {...queue} />);
-  return <FlipMove style={{ overflowAnchor: 'none' }}
-  maintainContainerHeight="true"
-  appearAnimation="fade"
-   enterAnimation="fade"
-    leaveAnimation="fade"
-     className="queue-list"
-      typeName="section"
-      className="queue-list">{queueArr}</FlipMove>;
+  return (
+    <FlipMove style={{ overflowAnchor: "none" }} maintainContainerHeight="true" appearAnimation="fade" enterAnimation="fade" leaveAnimation="fade" className="queue-list" typeName="section" className="queue-list">
+      {queueArr}
+    </FlipMove>
+  );
 }
-
-
