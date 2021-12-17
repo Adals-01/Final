a7940d5461c6b4370b0b359743c825013293e23f
Anna <anna355y@stud.kea.dk>
2021-12-17 06:47:59 +0100

    round sum

--- foobar/src/components/Order/stepone.jsx
+++ foobar/src/components/Order/stepone.jsx
@@ -4,9 +4,11 @@ export default function StepOne(props) {
   let sum = props.MyBasket.reduce(function (previousValue, currentValue) {
     return previousValue + currentValue.price;
   }, initialValue);
-  const newsum = sum.toString().split(".").join("");
-
+  const newsum = Math.round((sum*10) * 100) / 100
   
+
+
+
   const noDublicates = [...new Map(props.MyBasket.map((item) => [item.name, item])).values()];
   let basketItemsArr = noDublicates.map((MyBasket, index) => <BasketItem countTypeInBasket={props.countTypeInBasket} {...MyBasket} addToBasket={props.addToBasket} removeFromBasket={props.removeFromBasket} key={index} />);
 
@@ -15,7 +17,7 @@ export default function StepOne(props) {
       <div>
         {Array.isArray(basketItemsArr) && basketItemsArr.length === 0 ? <h2>There is nothing in the cart yet...</h2> : <h2>Here is what you have chosen:</h2>}
         <div>{basketItemsArr}</div>
-        <p> {props.MyBasket.length} items</p>
+      
 
       </div>
       <div className="bottom-cart">
