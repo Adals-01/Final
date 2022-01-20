62dc24bc9b5812b96f14acd17ecf8808dca8d124
Elise <aemccreary5@gmail.com>
2022-01-20 16:05:12 +0100

    price things

--- foobar/src/components/Order/beerlist.jsx
+++ foobar/src/components/Order/beerlist.jsx
@@ -1,6 +1,7 @@
 import Beerlistitem from "./beerlistitem";
 
 export default function Beerlist(props) {
+  console.log(props.dashboard.taps);
   const beerArr = props.data.map((product, index) => <Beerlistitem countTypeInBasket={props.countTypeInBasket} addToBasket={props.addToBasket} removeFromBasket={props.removeFromBasket} {...product} key={index} dashboard={props.dashboard.storage} />);
   return (
     <div className="Home">
--- foobar/src/components/Order/beerlistitem.jsx
+++ foobar/src/components/Order/beerlistitem.jsx
@@ -1,25 +1,53 @@
 import React from "react";
 import getImageByKey from "./getImageByKey";
 import PlusMinus from "./plusminus";
+import StorageItem from "./amount";
+// import React, { useState, useEffect } from "react";
+
+// const [arrayx, setArrayx] = UseState([]);
 
 export default function Beerlistitem(props) {
   const beer = props.name;
   let beerimg = beer.replace(/\s+/g, "").toLowerCase();
+
+  // function amount(myArray) {
+  //   let extractedArray = [];
+  //   for (let i = 0; i < myArray.length; i++) {
+  //     extractedArray.push(myArray[i]);
+  //   }
+  //   console.log("this is" + extractedArray);
+  // }
+
   function Price() {
     let storage = props.dashboard;
 
-    // finding lowest amount number in storage
-    let amountArr = [];
-    amountArr = storage.map((item) => <p>{item.amount}</p>);
-    console.log(amountArr);
+    //amount(JSON.parse(storage));
+    // finding highest amount number in storage
+    // let amountArr = [];
 
-    const min = Math.min(...amountArr.props.children);
-    console.log(min);
+    // let arr = [];
 
+    // console.log(amountArr);
+
+    // console.log(highinstock);
+    // const storageArr = storage.map((product, index) => <StorageItem {...product} key={index} dashboard={props.dashboard} />);
+
+    // function iterate(item) {
+    //   let amountArr = storage.map((item) => item.amount);
+    //   const highinstock = Math.max(...amountArr);
+    //   if (item.amount === highinstock) {
+    //     console.log(item.name + "+" + highinstock);
+    //     let newprice = `${price}` / 10;
+    //     return newprice;
+    //   }
+    //   return highinstock;
+    // }
+    // storage.forEach(iterate);
     //creating price
     const beer = props.name;
     if (beer === "El Hefe" || beer === "Hollaback Lager" || beer === "Hoppily Ever After" || beer === "Mowintime" || beer === "Row 26" || beer === "Steampunk") {
       let price = 65;
+
       return <p>{price}</p>;
     } else if (beer === "Fairy Tale Ale" || beer === "GitHop" || beer === "Ruined Childhood" || beer === "Sleighride") {
       let price = 75;
